f1e6ef616ffc9e05ba5b938ae3ae4558019e7625
João Batista da Rocha Neto <framework25@localiza.com>
2022-11-27 19:47:19 -0300

    Criação do formulário de cadastro

---
+++ src/app/conta/conta.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+import { ContaAppComponent } from './conta.app.component';
+
+import { ContaRoutingModule } from './conta.route';
+import { ContaService } from './services/conta.service';
+import { NarikCustomValidatorsModule } from '@narik/custom-validators';
+
+@NgModule({
+  declarations: [
+    CadastroComponent,
+    LoginComponent,
+    ContaAppComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ContaRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NarikCustomValidatorsModule
+  ],
+  providers: [
+    ContaService
+  ]
+})
+export class ContaModule { }
